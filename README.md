2ce3d484f9415ee5045902666a41ff2617d7cafa
atik007 <atikhasan1971arik@gmail.com>
2024-11-25 02:00:55 +0600

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # propertyPlus
+# propertyPlus
---
+++ src/components/Companies/Companies.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Companies.css';
+const Companies = () => {
+  return (
+    <section className="c-wrapper">
+      <div className="paddings innerWidth flexCenter c-container">
+        <img src="./l1.png" alt="" />
+        <img src="./l2.png" alt="" />
+        <img src="./l3.png" alt="" />
+        <img src="./l4.png" alt="" />
+      </div>
+    </section>
+  );
+};
+
+export default Companies;
